bc9d7bc8828ce7c1085b282fa32bd5d57b2c83b8
Phongldm <leminhphong03@gmail.com>
2024-11-01 20:34:43 +0700

    refactor: update user api controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,5 +1,7 @@
-﻿using API.Models;
+﻿using API.DTO;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -21,6 +23,100 @@ namespace API.Controllers
             }
 
             return Ok(user);
+        }
+
+        [HttpGet("List")]
+        public IActionResult GetUserList()
+        {
+            var users = context.Users.ToList();
+
+            return Ok(users);
+        }
+
+        [HttpGet(Name = "Login/{username}/{password}")]
+        public IActionResult GetUserById(string username, string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                return NotFound("Can't get parameter!");
+            }
+            else
+            {
+                User u = context.Users.FirstOrDefault(u => u.Username == username && u.Password == password);
+                if (u == null)
+                {
+                    return NotFound("User does not exist in database!");
+                }
+                else
+                {
+                    return Ok(u);
+                }
+
+
+            }
+        }
+
+
+        [HttpPost("Register")]
+        public async Task<IActionResult> RegisterUser([FromBody] UserRegistrationDto registrationDto)
+        {
+            if (registrationDto == null)
+            {
+                return BadRequest("Registration data is required.");
+            }
+
+            if (string.IsNullOrEmpty(registrationDto.Username) ||
+                string.IsNullOrEmpty(registrationDto.Password) ||
+                string.IsNullOrEmpty(registrationDto.FullName))
+            {
+                return BadRequest("All fields are required.");
+            }
+
+            // Check if user already exists
+            if (context.Users.Any(u => u.Username == registrationDto.Username))
+            {
+                return Conflict("Email address already in use.");
+            }
+
+            var newUser = new User
+            {
+                Username = registrationDto.Username,
+                Password = registrationDto.Password,
+                FullName = registrationDto.FullName,
+                BirthYear = registrationDto.BirthYear,
+            };
+
+            context.Users.Add(newUser);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUserById), new { username = newUser.Username, password = newUser.Password }, newUser);
+        }
+
+        [HttpPost("ForgetPassword")]
+        public async Task<IActionResult> ForgetPassword([FromBody] ForgetPasswordDto forgetPasswordDto)
+        {
+            if (forgetPasswordDto == null)
+            {
+                return BadRequest("Forget password data is required.");
+            }
+
+            if (string.IsNullOrEmpty(forgetPasswordDto.Username) || string.IsNullOrEmpty(forgetPasswordDto.NewPassword))
+            {
+                return BadRequest("Email address and new password are required.");
+            }
+
+            // Find the user by email address
+            var user = context.Users.FirstOrDefault(u => u.Username == forgetPasswordDto.Username);
+            if (user == null)
+            {
+                return NotFound("User does not exist.");
+            }
+
+            // Update the user's password (consider hashing the password before saving)
+            user.Password = forgetPasswordDto.NewPassword;
+            await context.SaveChangesAsync();
+
+            return Ok("Password updated successfully.");
         }
     }
 }
--- API/obj/Debug/net6.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net6.0/API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0cf13e48a1425e95d9cc14c3da97bd3e3abb112")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
